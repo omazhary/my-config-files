ec3671524e93a81cf57aeaf8b4c6c044cbb06fd3
Omar Elazhary <omazhary@gmail.com>
2018-03-08 16:45:58 -0800

    Add Z-Shell integration

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-./nerd-fonts/install.sh
+cp .bashrc ~/.
+cp .dircolors ~/.
+cp .git-prompt.sh ~/.
+cp .tmux.conf ~/.
+cp .tmuxinator.bash ~/.
+cp .vimrc ~/.
 cp az-conformant.zsh-theme .oh-my-zsh/themes/.
+cp .zshrc ~/.
