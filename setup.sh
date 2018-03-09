45146cb2d33a18276d4ddb051416e9c90d0f28af
Omar Elazhary <omazhary@gmail.com>
2018-03-09 00:12:56 -0800

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+sudo apt install -y fonts-powerline
+
+pushd /tmp
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+popd
+
 cp .bashrc ~/.
 cp .dircolors ~/.
 cp .git-prompt.sh ~/.
 cp .tmux.conf ~/.
 cp .tmuxinator.bash ~/.
 cp .vimrc ~/.
-cp az-conformant.zsh-theme .oh-my-zsh/themes/.
+cp az-conformant.zsh-theme ~/.oh-my-zsh/themes/.
 cp .zshrc ~/.
 
 git submodule init
 git submodule update
 
-sh nerd-fonts/install.sh
+./nerd-fonts/install.sh
