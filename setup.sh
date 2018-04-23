cce104c4a6dd7a204d9c1e0812b5603dd09b5b51
Omar Elazhary <omazhary@gmail.com>
2018-04-23 10:26:01 -0700

    Remove Zsh integration

--- setup.sh
+++ setup.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-sudo apt install -y fonts-powerline
-
-pushd /tmp
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-popd
-
 cp .bashrc ~/.
 cp .dircolors ~/.
 cp .git-prompt.sh ~/.
 cp .tmux.conf ~/.
 cp .tmuxinator.bash ~/.
 cp .vimrc ~/.
-cp az-conformant.zsh-theme ~/.oh-my-zsh/themes/.
-cp .zshrc ~/.
-
-git submodule init
-git submodule update
-
-./nerd-fonts/install.sh
-
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
