6429c380052edfa194d346c097bcbc0fca9f5718
Omar Elazhary <omazhary@gmail.com>
2018-05-10 23:32:00 -0700

    Automate native package installation

--- setup.sh
+++ setup.sh
@@ -1,4 +1,47 @@
-#!/bin/bash
+#!/bin/bash -e
+
+shopt -s nocasematch
+
+array_contains() {
+    local seeking=$2
+    name=$1[@]
+    local array=("${!name}")
+    local in=1
+    for element in "${array[@]}"; do
+        if [[ $element == $seeking ]]; then
+            in=0
+            break
+        fi
+    done
+    return $in
+}
+
+# Identify package manager
+declare -a yum=("CentOS" "Fedora")
+declare -a apt=("Debian" "Ubuntu", "Lubuntu", "Kubuntu", "Xubuntu")
+echo 'Identifying native package manager...'
+DISTRO=$(cat /etc/os-release | grep '^NAME=' | sed -En 's/NAME=//p')
+PACMAN=''
+if array_contains yum $DISTRO ; then
+    PACMAN="yum"
+elif array_contains apt $DISTRO ; then
+    PACMAN="apt"
+else
+    echo "Unidentified distro, aborting..."
+    exit 1
+fi
+echo "You're running ${DISTRO}."
+echo "The corresponding package manager is ${PACMAN}."
+
+# Install packages
+echo 'Installing additional packages...'
+packages=$(cat setuplist.csv)
+for package in $packages
+do
+    echo "### Installing $package..."
+    eval "sudo $PACMAN install -yq $package"
+    echo "$package installation done."
+done
 
 # Copy config files
 echo 'Copying over basic config files...'
@@ -12,7 +55,7 @@ cp .inputrc ~/.
 
 # Create fonts directory
 echo 'Copying over custom fonts...'
-mkdir ~/.fonts
+mkdir -p ~/.fonts
 
 # LaTeX document templates
 echo 'Copying over custom latex templates...'
